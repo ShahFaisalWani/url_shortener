ee2bd20866a1b45be19edda1c08a9eed8dcb4225
ShahFaisalWani <shahfaisalwani14@gmail.com>
2024-06-11 01:42:43 +0700

    fix deploy app domain

--- src/env.ts
+++ src/env.ts
@@ -3,8 +3,7 @@ dotenv.config();
 
 export const env = {
   PORT: process.env.PORT,
-  APP_DOMAIN: process.env.APP_DOMAIN,
-  APP_BASE_URL: `${process.env.APP_DOMAIN}:${process.env.PORT}`,
+  APP_BASE_URL: process.env.APP_BASE_URL,
   DB_HOST: process.env.DB_HOST,
   DB_USER: process.env.DB_USER,
   DB_PASS: process.env.DB_PASS,
